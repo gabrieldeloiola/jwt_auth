b4c34b2e7aa56082f39035262e31512ee0271a90
Gabriel Pessoa <tentandodrop@gmail.com>
2023-05-01 21:30:08 -0300

    Adding initial code/configurations

---
+++ src/main/java/br/com/gabrielpessoa/jwtauth/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.gabrielpessoa.jwtauth.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String password;
+
+}
